6992eee3eb1b44da1365d6e6b279e6b5d15bfd67
bflore11 <bflore11@asu.edu>
2019-10-08 22:33:42 -0700

    Initial commit of assignment 2

---
+++ assignment2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,63 @@
+package cse360assign2;
+
+/** 
+ * Name: Brando Flores
+ * Class ID: 70641
+ * This program sums and subtracts integers. It is used
+ * as an example of how to use version control.
+ * 
+ * @author Brando Flores
+ * @version 1.0
+ */
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * Constructor used to create AddingMachine objects.
+	 * Instantiated with total being 0.
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Get the current total of the AddingMachine object.
+	 * @return the total.
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Add an integer value to the current total.
+	 * @param the integer value to be added to the total.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtract an integer value form the current total.
+	 * @param the integer value to be subtracted from the total.
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * toString method returns the string representation of the
+	 * AddingMachine object.
+	 * @return the string representation of the object.
+	 */
+	public String toString () {
+		return "";
+	}
+	
+	/**
+	 * Have not designed use. May default total back to 0.
+	 */
+	public void clear() {
+	
+	}
+}
