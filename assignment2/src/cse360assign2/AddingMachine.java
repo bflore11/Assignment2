f46c9f9aff1707c5f5ad4e739c547044cbbcbdb1
bflore11 <bflore11@asu.edu>
2019-10-08 22:38:51 -0700

    Implemented functionality of blank methods.

--- assignment2/src/cse360assign2/AddingMachine.java
+++ assignment2/src/cse360assign2/AddingMachine.java
@@ -1,24 +1,28 @@
 package cse360assign2;
-
+      
 /** 
  * Name: Brando Flores
  * Class ID: 70641
- * This program sums and subtracts integers. It is used
- * as an example of how to use version control.
+ * This program sums and subtracts integers.
+ * It also keeps a history of any additions or subtractions made.
+ * It is used as an example of how to use version control.
  * 
  * @author Brando Flores
- * @version 1.0
+ * @version 2.0
  */
 public class AddingMachine {
 
 	private int total;
+	private String history;
 	
 	/**
 	 * Constructor used to create AddingMachine objects.
-	 * Instantiated with total being 0.
+	 * Instantiated with total being 0
+	 * and history being 0.
 	 */
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		history = "0";
 	}
 	
 	/**
@@ -26,38 +30,43 @@ public class AddingMachine {
 	 * @return the total.
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	/**
 	 * Add an integer value to the current total.
+	 * Update the history of the changes made to the total value.
 	 * @param the integer value to be added to the total.
 	 */
 	public void add (int value) {
-		
+		total += value;
+		history += " + " + value;	
 	}
 	
 	/**
 	 * Subtract an integer value form the current total.
+	 * Update the history of the changes made to the total value.
 	 * @param the integer value to be subtracted from the total.
 	 */
 	public void subtract (int value) {
-		
+		total -= value;
+		history += " - " + value;
 	}
 	
 	/**
-	 * toString method returns the string representation of the
-	 * AddingMachine object.
-	 * @return the string representation of the object.
+	 * toString method returns the history of additions 
+	 * and/or subtractions made to the total.
+	 * @return the history of changes made.
 	 */
 	public String toString () {
-		return "";
+		return history;
 	}
 	
 	/**
-	 * Have not designed use. May default total back to 0.
+	 * Sets total and history back to default values.
 	 */
 	public void clear() {
-	
+		total = 0;
+		history = "0";
 	}
 }
